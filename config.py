c0107ab4d6dca93dae72606688a5b3dac0601714
Aryan Garg <62931330+aryan0141@users.noreply.github.com>
2022-01-07 00:44:18 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# specify the path of our dataset
-CSV_PATH = "abalone_train.csv"
-
-# specify the column names of our dataframe
-COLS = ["Length", "Diameter", "Height", "Whole weight",
-	"Shucked weight", "Viscera weight", "Shell weight", "Age"]
